9b71dbd4e11ddab7ffb86477a52b200a2e45c2b0
thekidmellow <thekidmellow@gmail.com>
2025-05-23 16:44:57 +0200

    Added global game variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,4 +20,24 @@ const gameImages = {
         gameOver: '/assets/images/crying_face.png',
         background: '/assets/images/BG.jpg',
     }
-},
+};
+
+// Global game variables
+
+let currentQuestion = 0;
+let score = 0;
+let playerName = '';
+
+const questionText = document.getElementById('question-text');
+const options = document.querySelectorAll('.option');
+const quizScreen = document.getElementById('quiz-screen');
+const startScreen = document.getElementById('start-screen');
+const endScreen = document.getElementById('end-screen');
+const VictoryScreen = document.getElementById('victory-screen');
+
+const currentScore = document.getElementById('current-score');
+const finalScore = document.querySelectorAll('final-score');
+const finalUsername = document.getElementById('final-username');
+const victoryScore = document.getElementById('victory-score');
+const victoryUsername = document.getElementById('victory-username');
+
