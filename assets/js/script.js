83bc36c34176209f69c181de304c4a673f7c15a5
thekidmellow <thekidmellow@gmail.com>
2025-05-23 16:32:59 +0200

    Created object to preload and store all images

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Object to preload and store all images
+
+const gameImages = {
+    teams: {
+        celtics: '/assets/images/Boston.jpg',
+        bulls: '/assets/images/Bulls.jpg',
+        lakers: '/assets/images/L.A.jpg',
+        warriors: '/assets/images/Warriors.jpg',
+}
+    players: {
+        lebron: '/assets/images/LeBron.jpg',
+        kareem: '/assets/images/Kareem_Abdul-Jabbar.jpg',
+        malone: '/assets/images/Karl-Malone.jpg',
+        kobe: '/assets/images/Kobe.jpg',
+        jordan: '/assets/images/Michael-Jordan.jpg',
+        russell: '/assets/images/Bill_russell.jpg',
+    }
+    ui: {
+        victory: '/assets/images/correct_meme.png',
+        gameOver: '/assets/images/crying_face.png',
+        background: '/assets/images/BG.jpg',
+    }
+},
